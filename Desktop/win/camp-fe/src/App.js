204e44b181baab13e7ef5c76d391b44a51339756
HaoQQQQ <c0989167331@gmail.com>
2022-02-17 01:11:16 +0800

    20220217

---
+++ Desktop/win/camp-fe/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import CampDetail from "./components/CampDetail";
+import Footer from "./components/Footer";
+import Test from "./components/Test";
+import Test2 from "./components/Test2";
+import Booking from "./components/Booking";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      {/* <CampDetail /> */}
+      {/* <Footer /> */}
+      {/* <Test /> */}
+      {/* <Test2 /> */}
+      <Booking />
+    </>
+  );
+}
+
+export default App;
