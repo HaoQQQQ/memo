eaa6cb92608cbf012585408b5a8b4762f0717a70
HaoQQQQ <c0989167331@gmail.com>
2022-05-10 23:02:50 +0800

    練習

---
+++ Desktop/memo/my-app/src/pages/Home/components/Item.js
@@ -0,0 +1,21 @@
+const Item = ({ id, note, date, time, deleteDate, submittingStatus }) => {
+  function deleteItem() {
+    submittingStatus.current = true;
+    deleteDate(function (prev) {
+      return prev.filter((item) => item.id !== id);
+    });
+  }
+  return (
+    <div className="item">
+      <div>
+        <p>{note}</p>
+        <p>{`${date} ${time}`}</p>
+      </div>
+      <button onClick={deleteItem} className="remove">
+        刪除
+      </button>
+    </div>
+  );
+};
+
+export default Item;
