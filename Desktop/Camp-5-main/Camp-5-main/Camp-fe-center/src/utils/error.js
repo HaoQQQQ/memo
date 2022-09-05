b009cbc870b386cf391c7ccb628aced743fa48e0
hao <95270561+HaoQQQQ@users.noreply.github.com>
2022-09-05 17:26:12 +0800

    Delete Desktop/Camp-5-main/Camp-5-main directory

--- Desktop/Camp-5-main/Camp-5-main/Camp-fe-center/src/utils/error.js
+++
@@ -1,3 +0,0 @@
-export const ERR_MSG = {
-  33001: "email 格式錯誤",
-};
--- Desktop/Camp-5-main/Camp-5-main/member-center-be/routers/campPODetailTent.js
+++
@@ -1,9 +0,0 @@
-// 這裏 stock 的 router
-const express = require("express");
-const router = express.Router();
-const campPODetail = require("../controllers/campPODetail.js");
-
-
-router.get("/:POId", campPODetail.asyncCampOrderTent);
-
-module.exports = router;
